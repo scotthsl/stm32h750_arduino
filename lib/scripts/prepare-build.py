c38440b025dc63c46b001af18f6c18cda7ac90e6
Scott Huang <shili.huang@snapmaker.com>
2019-11-03 22:06:46 +0800

    NEW: add scripts to copy board speific dir to pio package variants dir
    Signed-off-by: Scott Huang <shili.huang@hotmail.com>

--- lib/scripts/prepare-build.py
+++ lib/scripts/prepare-build.py
@@ -1,39 +1,70 @@
 import shutil
 import sys
 from os.path import isdir, isfile, join
-
+import os
+from filecmp import dircmp
 Import("env")
 
 platform = env.PioPlatform()
 board = env.BoardConfig()
 
-print ("++++++++++++++++++++++++++++++Prepare env start++++++++++++++++++++++++++++++")
-print ("Prepare env for " + env.get("BOARD")
+print("++++++++++++++++++++++++++++++copy variant start++++++++++++++++++++++++++++++")
+print("Prepare env for " + env.get("BOARD"))
 
 frwk = env.get("PIOFRAMEWORK")
 pkg = platform.frameworks[frwk[0]]["package"]
 
 pkg_dir = platform.get_package_dir(pkg)
 
-print ("Package is " + pkg)
-print ("Location: " + pkg_dir)
+print("Package is " + pkg)
+print("Location: " + pkg_dir)
 
-mcu = board.get('build.mcu')
+proj_dir = env.get("PROJECT_DIR")
+variant = board.get("build.variant")
 
-"""
-build_script = "platformio-build-%s.py" % mcu[0:7]
-build_script_path = join(pkg_dir, "tools", build_script)
-shutil.copy(join(sys.path[0], build_script), build_script_path)
-"""
-"""
-frwk_dir = join(pkg_dir, mcu[0:6].upper())
-if isdir(frwk_dir):
-  print "%s is exist." % frwk_dir
-  # TODO: here need to add comparation between src and dst framework
-else:
-  print "%s is not exist." % frwk_dir
-  print "now create it"
-  shutil.copytree(join(sys.path[0], mcu[0:6].upper()), frwk_dir)
-"""
+# copy my variant dir to pio packages dir for every build
+my_variant = join(proj_dir, "lib", pkg, "variants", variant)
+if not os.path.isdir(my_variant):
+  raise Exception("project variant directory is not exist!")
+pkg_variant = join(pkg_dir, "variants", variant)
 
-print ("++++++++++++++++++++++++++++++Prepare env end++++++++++++++++++++++++++++++") 
+print("my variant dir: " + my_variant)
+print("target variant dir: " + pkg_variant)
+
+def sync_dir(src, dst):
+  if os.path.isdir(dst):
+    print("target variant is exist!")
+    # if target variant dir is exist, check whether it has update
+    dcmp = dircmp(src, dst)
+
+    if len(dcmp.diff_files) > 0:
+      print("Some files need to be updated:")
+      for f in dcmp.diff_files:
+        f = join(src, f)
+        print(f)
+        os.remove(f)
+        shutil.copy(f, dst)
+    if len(dcmp.left_only) > 0:
+      print("Some files need to be create:")
+      for f in dcmp.left_only:
+        f = join(src, f)
+        print(f)
+        shutil.copy(f, dst)
+    if len(dcmp.right_only) > 0:
+      print("Some files need to be removed:")
+      for f in dcmp.right_only:
+        f = join(dst, f)
+        print(f)
+        os.remove(f)
+  else:
+    print("target variant is not exist. will creat it!")
+    # target is not exist, copy src to dst
+    try:
+      shutil.copytree(src, dst)
+    except Exception as e:
+      print("Failed to copy dir: " + str(e))
+      raise
+
+sync_dir(my_variant, pkg_variant)
+
+print("++++++++++++++++++++++++++++++copy variant end++++++++++++++++++++++++++++++")
